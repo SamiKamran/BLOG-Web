2de51f430e41c8b7a97983a2dc26bfb734ed7346
Sami Kamran <samikamranyls@gmail.com>
2024-02-12 22:53:59 +0500

    create React app and Install tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+
+<h1>REACT APP</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>
+        SignIn
+
+      
+    </div>
+  )
+}
+
+export default SignIn
