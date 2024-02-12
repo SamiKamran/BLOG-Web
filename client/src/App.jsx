7044184cb64fbb3a84000e05806f572dc8da2dcc
Sami Kamran <samikamranyls@gmail.com>
2024-02-12 23:08:48 +0500

    Solve Problem of react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import DashBoard from "./pages/DashBoard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Project";
+import Header from "./components/Header";
 
 
+
+
+// react login register forgot password header and home page
 function App() {
-
   return (
-    <>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/projects" element={<Projects />} />
 
-<h1>REACT APP</h1>
-    </>
-  )
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
